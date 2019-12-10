d419551c796fbfbffd9bb391500a949cfccc40f3
Paul Luna <paulluna0215@gmail.com>
2019-12-09 21:17:51 -0800

    started using babel-loader and css-loader

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.write('hello, webpack');
+require('./styles.scss');
+
+class Car {
+    manufacturer (car) {
+        document.write(`I have a ${car}`)
+    }
+}
+
+const bmw = new Car;
+bmw.manufacturer('bmw');
